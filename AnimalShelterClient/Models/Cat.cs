fd3e9aed45567bddb6453c38431d952d2777cf20
Ryan Walker <ryandanielwalker@gmail.com>
2021-06-11 13:45:54 -0700

    Add Dog model, add Get methods

--- AnimalShelterClient/Models/Cat.cs
+++ AnimalShelterClient/Models/Cat.cs
@@ -12,7 +12,7 @@ namespace AnimalShelterClient.Models
     public int Age { get; set; }
     public string Gender { get; set; }
     public string Description { get; set; }
-    private static string _component = "component";
+    private static string _component = "cat";
 
     public static List<Cat> GetCats()
     {
---
+++ AnimalShelterClient/Models/Dog.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace AnimalShelterClient.Models
+{
+  public class Dog
+  {
+    public int DogId { get; set; }
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public string Gender { get; set; }
+    public string Description { get; set; }
+    private static string _component = "dog";
+
+    public static List<Dog> Getdogs()
+    {
+      var apiCallTask = ApiHelper.GetAll(_component);
+      var result = apiCallTask.Result;
+
+      JArray jsonResponse = JsonConvert.DeserializeObject<JArray>(result);
+      List<Dog> dogList = JsonConvert.DeserializeObject<List<Dog>>(jsonResponse.ToString());
+
+      return dogList;
+    }
+    public static Dog GetDetails(int id)
+    {
+      var apiCallTask = ApiHelper.Get(_component, id);
+      var result = apiCallTask.Result;
+
+      JObject jsonResponse = JsonConvert.DeserializeObject<JObject>(result);
+      Dog dog = JsonConvert.DeserializeObject<Dog>(jsonResponse.ToString());
+
+      return dog;
+    }
+  }
+}
