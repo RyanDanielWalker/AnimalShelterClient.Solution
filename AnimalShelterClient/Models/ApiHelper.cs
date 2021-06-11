28dba40ee0f88e1019cf24702c32504e106d0a69
Ryan Walker <ryandanielwalker@gmail.com>
2021-06-11 13:18:09 -0700

    Add Put, Post, Delete methods to API helper

--- AnimalShelterClient/Models/ApiHelper.cs
+++ AnimalShelterClient/Models/ApiHelper.cs
@@ -19,5 +19,28 @@ namespace AnimalShelterClient.Models
       var response = await client.ExecuteTaskAsync(request);
       return response.Content;
     }
+    public static async Task Post(string component, string newObject)
+    {
+      RestClient client = new RestClient("http://localhost:5000/api");
+      RestRequest request = new RestRequest($"{component}", Method.POST);
+      request.AddHeader("Content-Type", "application/json");
+      request.AddJsonBody(newObject);
+      var response = await client.ExecuteTaskAsync(request);
+    }
+    public static async Task Put(string component, int id, string newObject)
+    {
+      RestClient client = new RestClient("http://localhost:5000/api");
+      RestRequest request = new RestRequest($"{component}/{id}", Method.PUT);
+      request.AddHeader("Content-Type", "application/json");
+      request.AddJsonBody(newObject);
+      var response = await client.ExecuteTaskAsync(request);
+    }
+    public static async Task Delete(string component, int id)
+    {
+      RestClient client = new RestClient("http://localhost:5000/api");
+      RestRequest request = new RestRequest($"{component}/{id}", Method.DELETE);
+      request.AddHeader("Content-Type", "application/json");
+      var response = await client.ExecuteTaskAsync(request);
+    }
   }
 }
